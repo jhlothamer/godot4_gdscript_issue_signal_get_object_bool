e97133dcb82a2de86eb6d3179c23c2503e6c51be
Jason Lothamer <jhlothamer@gmail.com>
2023-04-04 17:19:50 -0400

    simply reproducing code

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # Godot 4 Project - Reproduce Signal.get_object() Returns Bool Issue
 This Godot 4 project reproduces an issue I found with calling the get_object() method of a signal inside an if statement expression where it returns a bool value.  After that, it continues to evaluate to bool.
 
-Also demonstrated is that if the get_object() method is called before using it's return value in an if statement, it returns the expected object.
-
-This is done using a button, and two timers.  The button is just used as a signal source and because it's a node.  This mimics the conditions and code I original found the issue with.
-
-The timer timeout handler functions demonstrate the issue (Timer1) and the very simple work around (Timer2).
-
-The issue is not that it's hard to work around.  It's that it's very odd that the Signal.get_object() method returns a bool ever and makes me wonder if there are some more problematic issues going on.
--- project/recreate_scene.gd
+++ project/recreate_scene.gd
@@ -1,39 +1,15 @@
 extends Control
 
-
-@onready var _btn: Button = $Button
-
-
 var _signal: Signal
 
-
 func _ready():
-	_signal = _btn.pressed
-
-
-# Issue: Somehow the call to Signal.get_object() made in an if statement is returning a bool.  And then it continues to return bool from then on.
-func _on_timer_1_timeout():
-	# signal object should be valid (button not freed or anything) and it is a node
+	_signal = $Button.pressed
 	if is_instance_valid(_signal.get_object()) and (_signal.get_object() is Node):
-		print("Timer1: issue not reproduced") # if you see this message it's either fixed or I'm full of it
+		print("_ready: issue not reproduced") # if you see this message it's either fixed or I'm full of it
 	else:
 		# output parts of the if expression and to do some sanity checks
-		print("Timer1: issue reproduce (expected)")
-		print("Timer1: is_instance_valid(_signal.get_object()): %s, _signal.get_object() is Node: %s" % [is_instance_valid(_signal.get_object()), _signal.get_object() is Node])
-		print("Timer1: _signal.get_object(): %s" % [_signal.get_object()])
-		var temp = _signal.get_object()
-		print("Timer1: Is _signal.get_object() really a bool??? %s" % (typeof(temp) == TYPE_BOOL))
-
-
-# Issue work around:  Just call signal.get_object() before using in if statement.  No big deal.  But what other issues could this odd problem cause?
-func _on_timer_2_timeout():
-	var signal_object = _signal.get_object()
-	# signal object should be valid (button not freed or anything) and it is a node
-	if is_instance_valid(signal_object) and (signal_object is Node):
-		print("Timer2: issue not reproduced (expected)") # this is expected - the rest just mirrors code above
-	else:
-		# output parts of the if expression and to do some sanity checks
-		print("Timer2: issue reproduce")
-		print("Timer2: is_instance_valid(signal_object): %s, signal_object is Node: %s" % [is_instance_valid(signal_object), signal_object is Node])
-		print("Timer2: _signal.get_object(): %s" % [_signal.get_object()])
-		print("Timer2: Is _signal.get_object() really a bool??? %s" % (typeof(signal_object) == TYPE_BOOL))
+		print("_ready: issue reproduce (expected)")
+		print("_ready: is_instance_valid(_signal.get_object()) evalutes to %s" % is_instance_valid(_signal.get_object()))
+		print("_ready: _signal.get_object() is Node evalutes to %s" % _signal.get_object() is Node)
+		print("_ready: _signal.get_object() evalutes to %s" % _signal.get_object())
+		print("_ready: typeof(_signal.get_object()) == TYPE_BOOL ?? %s" % (typeof(_signal.get_object()) == TYPE_BOOL))
